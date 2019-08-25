5a69229e84a45276c31da19970accec315ee3c8f
Emily <a62262002@gmail.com>
2019-08-25 21:28:34 +0800

    restaurant list create

---
+++ README.md
@@ -0,0 +1,54 @@
+# 我的餐廳清單
+
+運用 node.js 和 express 進行開發，呈現 8 家餐廳清單，並具有餐廳詳細選單及搜尋餐廳之功能。
+
+## 安裝環境
+
+- Windows 8
+- Node.js v10.15.0
+- Express ^4.17.1
+
+## 安裝
+
+1. 下載 restaurant_list 資料夾，並輸入以下指令下載
+
+```
+$ git clone https://github.com/a62262002/restaurant_list.git
+```
+
+2. 從終端機安裝 npm 套件，輸入以下指令
+
+```
+$ npm install
+```
+
+3. 執行專案
+
+```
+$ npm run dev
+```
+
+4. 輸入網址並呈現我的餐廳清單介面
+
+```
+http://localhost:3000
+```
+
+## 功能說明
+
+- 顯示餐廳所有清單：http://localhost:3000
+- 顯示個別餐廳詳細介紹：http://localhost:3000/id
+- 搜尋餐廳：http://localhost:3000/search?keyword=搜尋輸入內容
+
+## 專案內容
+
+- 顯示餐廳所有清單
+  ![image](https://github.com/a62262002/restaurant_list/blob/master/restaurant_list.png)
+- 顯示個別餐廳詳細介紹
+  ![image](https://github.com/a62262002/restaurant_list/blob/master/restaurant_list_index.png)
+- 搜尋餐廳
+  ![image](https://github.com/a62262002/restaurant_list/blob/master/restaurant_list_index_research.png)
+
+## 作者
+
+[Emily Liu](https://github.com/a62262002)
---
+++ app.js
@@ -0,0 +1,43 @@
+// require pacjages used in the project
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// require express-HTMLHeadingElement;ebars here
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+
+// setting templates engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// setting static files
+app.use(express.static("public"));
+
+// route setting
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+
+// show
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  console.log("req.params.restaurant_id", req.params.restaurant_id);
+  const restaurant = restaurantList.results.find(
+    restaurant => restaurant.id.toString() === req.params.restaurant_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+
+// search
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase());
+  });
+  res.render("index", { restaurants: restaurants, keyword: keyword });
+});
+
+// start and listening on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`);
+});
