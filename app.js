0bc7bbe1ad6ccb32ee2158e2bd13e815bdffb651
Emily <a62262002@gmail.com>
2019-10-22 21:27:42 +0800

    Add edit page and update action

--- app.js
+++ app.js
@@ -89,12 +89,30 @@ app.post("/restaurants", (req, res) => {
 
 // 修改Restaurant頁面
 app.get("/restaurants/:id/edit", (req, res) => {
-  res.send("修改Retaurant頁面");
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    return res.render('edit', { restaurant: restaurant })
+  })
 });
 
 // 修改Restaurant
 app.post("/restaurants/:id/edit", (req, res) => {
-  res.send("修改Restaurant");
+  Restaurant.findById(req.params.id, (err, restaurant) => {
+    if (err) return console.error(err)
+    restaurant.name = req.body.name
+    restaurant.name_en = req.body.name_en
+    restaurant.category = req.body.category
+    restaurant.image = req.body.image
+    restaurant.location = req.body.location
+    restaurant.phone = req.body.phone
+    restaurant.google_map = req.body.google_map
+    restaurant.rating = req.body.rating
+    restaurant.description = req.body.description
+    restaurant.save(err => {
+      if (err) return console.error(err)
+      return res.redirect(`/restaurants/${req.params.id}`)
+    })
+  })
 });
 
 // 刪除Restaurant
