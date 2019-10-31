9bc7bc8b038ff25e3915f05d49855a8456e534ea
Emily <a62262002@gmail.com>
2019-10-31 19:38:28 +0800

    edit search

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const app = express();
 const mongoose = require("mongoose");
 const port = 3000;
 
-mongoose.connect("mongodb://localhost/restaurant", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/restaurant", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 
 // require express-HTMLHeadingElement;ebars here
 const exphbs = require("express-handlebars");
 
 // 引用body-parser
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 // 設定bodyParser
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // 連線異常
 db.on("error", () => {
@@ -38,33 +41,32 @@ app.use(express.static("public"));
 // restaurant首頁
 app.get("/", (req, res) => {
   Restaurant.find((err, restaurants) => {
-    if (err) return console.error(err)
-    return res.render('index', { restaurants: restaurants })
-  })
+    if (err) return console.error(err);
+    return res.render("index", { restaurants: restaurants });
+  });
   // res.render("index", { restaurants: restaurantList.results });
 });
 
 // 列出全部Todo
 app.get("/restaurants", (req, res) => {
-  return res.redirect('/')
+  return res.redirect("/");
 });
 
 // 新增一筆restaurant頁面
 app.get("/restaurants/new", (req, res) => {
-  return res.render('new')
+  return res.render("new");
 });
 
 // 顯示一筆Restaurant的詳細內容
 app.get("/restaurants/:id", (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
-    if (err) return console.error(err)
-    return res.render('detail', { restaurant: restaurant })
-  })
+    if (err) return console.error(err);
+    return res.render("detail", { restaurant: restaurant });
+  });
 });
 
 // 新增一筆Restaurant
 app.post("/restaurants", (req, res) => {
-
   // 建立restaurant model
   const restaurant = new Restaurant({
     name: req.body.name,
@@ -75,67 +77,76 @@ app.post("/restaurants", (req, res) => {
     phone: req.body.phone,
     google_map: req.body.google_map,
     rating: req.body.rating,
-    description: req.body.description,
-  })
+    description: req.body.description
+  });
 
   // 存入資料庫
   restaurant.save(err => {
-    if (err) return console.error(err)
+    if (err) return console.error(err);
 
     // 新增完成導回首頁
-    return res.redirect('/')
-  })
+    return res.redirect("/");
+  });
 });
 
 // 修改Restaurant頁面
 app.get("/restaurants/:id/edit", (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
-    if (err) return console.error(err)
-    return res.render('edit', { restaurant: restaurant })
-  })
+    if (err) return console.error(err);
+    return res.render("edit", { restaurant: restaurant });
+  });
 });
 
 // 修改Restaurant
 app.post("/restaurants/:id/edit", (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
-    if (err) return console.error(err)
-    restaurant.name = req.body.name
-    restaurant.name_en = req.body.name_en
-    restaurant.category = req.body.category
-    restaurant.image = req.body.image
-    restaurant.location = req.body.location
-    restaurant.phone = req.body.phone
-    restaurant.google_map = req.body.google_map
-    restaurant.rating = req.body.rating
-    restaurant.description = req.body.description
+    if (err) return console.error(err);
+    restaurant.name = req.body.name;
+    restaurant.name_en = req.body.name_en;
+    restaurant.category = req.body.category;
+    restaurant.image = req.body.image;
+    restaurant.location = req.body.location;
+    restaurant.phone = req.body.phone;
+    restaurant.google_map = req.body.google_map;
+    restaurant.rating = req.body.rating;
+    restaurant.description = req.body.description;
     restaurant.save(err => {
-      if (err) return console.error(err)
-      return res.redirect(`/restaurants/${req.params.id}`)
-    })
-  })
+      if (err) return console.error(err);
+      return res.redirect(`/restaurants/${req.params.id}`);
+    });
+  });
 });
 
 // 刪除Restaurant
 app.post("/restaurants/:id/delete", (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
-    if (err) return console.error(err)
+    if (err) return console.error(err);
     restaurant.remove(err => {
-      if (err) return console.log(err)
-      return res.redirect('/')
-    })
-  })
+      if (err) return console.log(err);
+      return res.redirect("/");
+    });
+  });
 });
 
 // search
 app.get("/search", (req, res) => {
   const keyword = req.query.keyword;
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase());
+  const regexp = new RegExp(keyword, "i");
+
+  Restaurant.find((err, collection) => {
+    if (err) return console.error(err);
+    const restaurants = collection.filter(item => item.name.match(regexp));
+    res.render("index", { restaurants: restaurants, keyword: keyword });
   });
-  res.render("index", { restaurants: restaurants, keyword: keyword });
+
+  // const restaurants = Restaurant.results.filter(restaurants => {
+  //   return restaurants.name.toLowerCase().includes(keyword.toLowerCase());
+  // });
 });
 
 // start and listening on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
 });
+
+console.log(Restaurant);
